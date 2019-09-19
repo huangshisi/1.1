bce8cab6ff99bdec177e4eb4bb2d0c9a583ef999
1625941327 <1625941327@qq.com>
2019-09-19 13:56:21 +0800

    1

--- 1.1/Program.cs
+++ 1.1/Program.cs
@@ -9,13 +9,16 @@ namespace _1._1
             var name = "bugs bunny";
             var age = 25;
             var isRabbit = true;
+            var a = 3.14;
 
             Type nameType = name.GetType();
             Type ageType = age.GetType();
             Type isRabbitType = isRabbit.GetType();
+            Type aType = a.GetType();
             Console.WriteLine("name is type" + nameType.ToString());
             Console.WriteLine("age is type" + ageType.ToString());
             Console.WriteLine("isRabbit is true" + isRabbitType.ToString());
+            Console.WriteLine("a is type"+aType .ToString ());
         }
     }
 }
